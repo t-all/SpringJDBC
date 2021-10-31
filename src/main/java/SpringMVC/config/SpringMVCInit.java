9ab1f3743eead3cd6add3c6d9d6b67f87e97915d
t-all <t-all@yandex.ru>
2021-10-31 11:13:14 +0300

    1

--- src/main/java/SpringMVC/config/SpringMVCInit.java
+++ src/main/java/SpringMVC/config/SpringMVCInit.java
@@ -1,6 +1,10 @@
 package SpringMVC.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class SpringMVCInit  extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -17,5 +21,16 @@ public class SpringMVCInit  extends AbstractAnnotationConfigDispatcherServletIni
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
